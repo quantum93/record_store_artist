5b4dc8646b93d548f835a93d2c3568c86c6764a4
Ben Bennett-Cauchon and Tae Lee <tae@>
2019-07-30 11:52:38 -0700

    This is update with database backup.

--- record_store.sql
+++ record_store.sql
@@ -6,3 +6,11 @@ CREATE TABLE artists (id serial PRIMARY KEY, name varchar);
 CREATE TABLE albums_artists (id serial PRIMARY KEY, artist_id int, album_id int);
 DROP DATABASE record_store_test;
 CREATE DATABASE record_store_test WITH TEMPLATE record_store;
+
+-- To back up your database, go to the root directory of your project in the terminal. Run the following command in the bash shell (not in psql):
+pg_dump [YOUR DATABASE NAME] > database_backup.sql
+
+-- Now that we have a backup, we can quickly create a fresh database that includes our backup schema. 
+createdb [DATABASE NAME]
+psql [DATABASE_NAME] < database_backup.sql
+createdb -T [DATABASE NAME] [TEST DATABASE NAME]
